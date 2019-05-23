83190545a090b7fb210981e3bba70c46533eca2e
Fallensouls <122584482@qq.com>
2019-05-23 17:01:37 +0800

    complete rpcs of shard master

--- shardmaster/server.go
+++ shardmaster/server.go
@@ -1,6 +1,7 @@
 package shardmaster
 
 import (
+	"log"
 	"sync"
 
 	"github.com/Fallensouls/raft/labgob"
@@ -16,6 +17,7 @@ type ShardMaster struct {
 
 	// Your data here.
 	executed map[string]uint64
+	done     chan int
 	configs  []Config // indexed by config num
 }
 
@@ -28,14 +30,14 @@ type Op struct {
 
 func (sm *ShardMaster) Join(args *JoinArgs, reply *JoinReply) {
 	// Your code here.
-	sm.mu.Lock()
-	defer sm.mu.Unlock()
 	if sm.rf.State() != raft.Leader {
 		reply.WrongLeader = true
 		return
 	}
+	sm.mu.Lock()
 	newConfig := Config{Groups: make(map[int][]string)}
 	oldConfig := sm.configs[len(sm.configs)-1]
+	sm.mu.Unlock()
 	for key, value := range oldConfig.Groups {
 		newConfig.Groups[key] = value
 	}
@@ -44,19 +46,32 @@ func (sm *ShardMaster) Join(args *JoinArgs, reply *JoinReply) {
 	}
 	newConfig.Num = oldConfig.Num + 1
 	newConfig.Shards = oldConfig.Shards
-	sm.rf.Start(Op{Config: newConfig, ID: args.ID, Seq: args.Seq})
+	index, _, _ := sm.rf.Start(Op{Config: newConfig, ID: args.ID, Seq: args.Seq})
+	for {
+		select {
+		case doneIndex := <-sm.done:
+			log.Println(doneIndex)
+			if doneIndex == index {
+				return
+			}
+			//case <-timeout.C:
+			//	timeout.Stop()
+			//	res.Err = ErrTimeout
+			//	return
+		}
+	}
 }
 
 func (sm *ShardMaster) Leave(args *LeaveArgs, reply *LeaveReply) {
 	// Your code here.
-	sm.mu.Lock()
-	defer sm.mu.Unlock()
 	if sm.rf.State() != raft.Leader {
 		reply.WrongLeader = true
 		return
 	}
+	sm.mu.Lock()
 	newConfig := Config{Groups: make(map[int][]string)}
 	oldConfig := sm.configs[len(sm.configs)-1]
+	sm.mu.Unlock()
 	for key, value := range oldConfig.Groups {
 		newConfig.Groups[key] = value
 	}
@@ -65,50 +80,94 @@ func (sm *ShardMaster) Leave(args *LeaveArgs, reply *LeaveReply) {
 	}
 	newConfig.Num = oldConfig.Num + 1
 	newConfig.Shards = oldConfig.Shards
-	sm.rf.Start(Op{Config: newConfig, ID: args.ID, Seq: args.Seq})
+	index, _, _ := sm.rf.Start(Op{Config: newConfig, ID: args.ID, Seq: args.Seq})
+	//timeout := time.NewTimer(10 * raft.HeartBeatInterval)
+	for {
+		select {
+		case doneIndex := <-sm.done:
+			if doneIndex == index {
+				return
+			}
+			//case <-timeout.C:
+			//	timeout.Stop()
+			//	res.Err = ErrTimeout
+			//	return
+		}
+	}
 }
 
 func (sm *ShardMaster) Move(args *MoveArgs, reply *MoveReply) {
 	// Your code here.
-	sm.mu.Lock()
-	defer sm.mu.Unlock()
 	if sm.rf.State() != raft.Leader {
 		reply.WrongLeader = true
 		return
 	}
+	sm.mu.Lock()
 	newConfig := Config{Groups: make(map[int][]string)}
 	oldConfig := sm.configs[len(sm.configs)-1]
+	sm.mu.Unlock()
 	for key, value := range oldConfig.Groups {
 		newConfig.Groups[key] = value
 	}
 	newConfig.Num = oldConfig.Num + 1
 	newConfig.Shards = oldConfig.Shards
 	newConfig.Shards[args.Shard] = args.GID
-	sm.rf.Start(Op{Config: newConfig, ID: args.ID, Seq: args.Seq})
+	index, _, _ := sm.rf.Start(Op{Config: newConfig, ID: args.ID, Seq: args.Seq})
+	for {
+		select {
+		case doneIndex := <-sm.done:
+			if doneIndex == index {
+				return
+			}
+			//case <-timeout.C:
+			//	timeout.Stop()
+			//	res.Err = ErrTimeout
+			//	return
+		}
+	}
 }
 
 func (sm *ShardMaster) Query(args *QueryArgs, reply *QueryReply) {
 	// Your code here.
-	sm.mu.Lock()
-	defer sm.mu.Unlock()
 	if sm.rf.State() != raft.Leader {
 		reply.WrongLeader = true
 		return
 	}
-	reply.Config = sm.configs[args.Num]
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	if sm.rf.Read() != nil {
+		reply.WrongLeader = true
+		return
+	}
+	if args.Num == -1 || args.Num >= len(sm.configs) {
+		reply.Config = sm.configs[len(sm.configs)-1]
+	} else {
+		reply.Config = sm.configs[args.Num]
+	}
+	log.Printf("reply of query: %v", reply)
+}
+
+func (sm *ShardMaster) assignShards() {
+	//latestConfig := sm.configs[len(sm.configs)-1]
+	//avarage := NShards / len(latestConfig.Groups)
+	//remainder := NShards % len(latestConfig.Groups)
+	//
 }
 
 func (sm *ShardMaster) apply() {
 	for {
 		select {
 		case msg, ok := <-sm.applyCh:
-			if ok && msg.NoOpCommand {
+			if ok && !msg.NoOpCommand {
 				if op, ok := msg.Command.(Op); ok {
 					if sm.executed[op.ID] < op.Seq {
 						sm.mu.Lock()
 						sm.configs = append(sm.configs, op.Config)
 						sm.executed[op.ID] = op.Seq
 						sm.mu.Unlock()
+						if sm.rf.State() == raft.Leader && !msg.Recover {
+							sm.done <- msg.CommandIndex
+						}
 					}
 				}
 			}
@@ -150,6 +209,9 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister)
 	sm.rf = raft.Make(servers, me, persister, sm.applyCh)
 
 	// Your code here.
+	sm.done = make(chan int, 100)
+	sm.executed = make(map[string]uint64)
+	go sm.apply()
 
 	return sm
 }
